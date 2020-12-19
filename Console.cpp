e416585df0af7f24b50a14223d7b1ef9f5e32c54
tartarus-git <niktedig@gmail.com>
2020-12-19 16:28:13 +0100

    Other function was obsolete, this one works better anyways. Added time markers, still have to clean code though.

--- Console.cpp
+++ Console.cpp
@@ -5,31 +5,29 @@
 #include <stdio.h>
 #include <ctime>
 
+#define TIME_MARKER_INIT_SIZE 512 // Check if there is some sort of standard for this.
+
 void Console::log(const char* message) {
 	size_t length = strlen(message);
 
 	// Get the current time.
 	const std::time_t currentTime = std::time(0);
-        const char* timeMarker = std::ctime(&currentTime);
+	tm* localCurrentTime = localtime(&currentTime);
+	char timeMarker[TIME_MARKER_INIT_SIZE]; // It would be better if this were a static array that's always kept around.
+	strftime(timeMarker, TIME_MARKER_INIT_SIZE, "[%c] ", localCurrentTime);
+	// Get the length of the time marker up to the NUL character.
 	size_t timeLength = strlen(timeMarker);
-	// This stuff is obsolete, use the new function instead.
 
 	// Create a SafePointer to a new buffer, which will be used to hold the edited message.
 	// Make the length accomodate the time marker, the message, the newline and the NUL character.
-	SafePointer<char*> buffer(new char[1 + timeLength + 2 + length + 2], [](char* handle) { delete[] handle; });
+	SafePointer<char*> buffer(new char[timeLength + length + 2], [](char* handle) { delete[] handle; });
 	// I could of used unique_ptr for this, and maybe I should, I just forgot about it. This works just as well though doesn't it?
 
 	// Copy time marker to buffer.
-	*buffer.handle = '[';
-	char* timeMarkerStart = buffer.handle + 1;
-	std::memcpy(timeMarkerStart, timeMarker, timeLength);
-	char* timeMarkerEnd = timeMarkerStart + timeLength;
-	*timeMarkerEnd = ']';
-	timeMarkerEnd += 1;
-	*(timeMarkerEnd) = ' ';
+	std::memcpy(buffer.handle, timeMarker, timeLength);
 
 	// Copy existing data to the buffer.
-	char* messageStart = timeMarkerEnd + 1;
+	char* messageStart = buffer.handle + timeLength;
 	std::memcpy(messageStart, message, length);
 
 	// Add the newline and the NUL characters.
