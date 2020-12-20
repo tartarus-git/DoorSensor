720fdb88eb540998e5c9b361b92be6d5aa82d1fb
tartarus-git <niktedig@gmail.com>
2020-12-20 19:06:10 +0100

    Currently polishing. Try 7 instead of 8. Almost there.

--- LifetimeLog.cpp
+++ LifetimeLog.cpp
@@ -18,22 +18,20 @@ bool LifetimeLog::start() {
 	if (f.is_open()) {
 		// Get the current time and output it to the lifetime log to announce startup.
 		char timeStamp[TIME_STAMP_INIT_SIZE];
-		for (int i = 0; i < TIME_STAMP_INIT_SIZE; i++) {
-			timeStamp[i] = 0;
-		}
 		const std::time_t currentTime = std::time(0);
-        	std::strftime(timeStamp, TIME_STAMP_INIT_SIZE, "\n%c\n", std::localtime(&currentTime));
+        	std::strftime(timeStamp, TIME_STAMP_INIT_SIZE, "\n%c\n\0\0\0\0\0\0\0\0", std::localtime(&currentTime));
 		//f << timeStamp;
 		f.write(timeStamp, TIME_STAMP_INIT_SIZE);
 		// Consider popping timeStamp from the stack once this is over with. As long as it doesn't get optimized that is.
+		// Ask on StackOverflow how to achieve this. You might have to use inline assembly language.
 		std::cout << sizeof(unsigned long long) << std::endl;
 		lifetimeThread = std::thread([]() {
+			unsigned long long counter;
 			while (isAlive) {
 				std::this_thread::sleep_for(std::chrono::seconds(5));
 
 				// Increment lifetime counter to report uptime.
 				f.seekg(-8, std::ios::end);
-				unsigned long long counter = 0x4100000000000000;
 				f.read((char*)(&counter), 8);
 				std::cout << std::hex << counter << std::endl;
 				counter++;
