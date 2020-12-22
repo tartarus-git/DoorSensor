0a0886d661dc8242101c696f949627973cc4fd35
tartarus-git <niktedig@gmail.com>
2020-12-22 20:35:22 +0100

    Trying to do the file thing with stat.h

--- Camera.cpp
+++ Camera.cpp
@@ -2,11 +2,13 @@
 
 #include <opencv2/opencv.hpp>
 #include <thread>
-#include <sys/stat.h> // Use this for the file size measurements.
+#include <sys/stat.h>
 #include <chrono>
 
 #define PLAYBACK_FPS 25
 #define CAPTURE_INTERVAL 40
+
+#define OUTPUT_PATH "../output.avi"
 
 bool Camera::failure;
 
@@ -15,7 +17,7 @@ bool Camera::init() {
 	cap = cv::VideoCapture(0);
 	if (cap.isOpened()) {
         	// Create VideoWriter object and set codec, fps, etc...
-        	output = cv::VideoWriter("../output.avi", cv::VideoWriter::fourcc('X', '2', '6', '4'), PLAYBACK_FPS, 
+        	output = cv::VideoWriter(OUTPUT_PATH, cv::VideoWriter::fourcc('X', '2', '6', '4'), PLAYBACK_FPS, 
 			cv::Size(cap.get(cv::CAP_PROP_FRAME_WIDTH), cap.get(cv::CAP_PROP_FRAME_HEIGHT)));
 		// Find a way to validate the VideoWriter.
 		failure = false;
@@ -34,13 +36,17 @@ void Camera::record() {
 	recordThread = std::thread([]() {
 		// Stream video into file.
 		cv::Mat frame;
+		struct stat statBuf;
 		while (isAlive) {
                 	cap >> frame;
                 	output.write(frame);
+
+			// Make sure the file size doesn't get out of hand.
+			stat(OUTPUT_PATH, &statBuf);
+			printf("%d", (int)statBuf.st_blocks);
+
                 	std::this_thread::sleep_for(std::chrono::milliseconds(10));
         	}
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(CAPTURE_INTERVAL));
 	});
 }
 
